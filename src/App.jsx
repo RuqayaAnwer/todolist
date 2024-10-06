89f475b68e942f18e440dfb247244a7f4ba33e12
ruqayaanwar <ruqayaanwer5@gmail.com>
2024-10-06 21:59:07 +0300

    to-do-list

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/Todo';
+
+const App =  () =>{
+  return <div className='container'>
+      <div>
+        <Todo/>
+      </div>
+
+
+    </div>
+
+  
+  
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+
+
+
+function Todo() {
+    const [tasks, setTasks]=useState(["eat breake fast", "take shower", "wallk"]);
+    const [newTask,setNewTask]=useState("");
+    function handelInputChange(event){
+        setNewTask(event.target.value);
+
+    }
+
+    function addTask(){
+        if(newTask.trim() !==""){
+            setTasks(t =>[...t, newTask]);
+            setNewTask("")
+    
+        }
+       
+    }
+
+
+    function deletTask(index){
+        const updatedTask =tasks.filter((_, i) => i !== index);
+        setTasks(updatedTask);
+
+    }
+
+    function moveTaskUp(index){
+        if(index > 0){
+            const updatedTask =[...tasks];
+            [updatedTask[index],updatedTask[index - 1]] =
+            [updatedTask[index-1], updatedTask[index]];
+            setTasks(updatedTask);
+        }
+
+    }
+    function moveTaskDown(index){
+        if(index < tasks.length -1){
+            const updatedTask =[...tasks];
+            [updatedTask[index],updatedTask[index + 1]] =
+            [updatedTask[index + 1], updatedTask[index]];
+            setTasks(updatedTask);
+        }
+
+    }
+
+  return (
+    <div className='to-do-list'>
+        <h1>To-Do-List</h1>
+        <div>
+            <input
+            type='text'
+            placeholder='Enter a Task...'
+            value={newTask}
+            onChange={handelInputChange}/>
+                <button className='add-button'
+    onClick={addTask}>
+        Add
+
+    </button>
+        </div>
+        <ol>
+            {tasks.map((task,index)=> 
+            <li key={index}>
+                <span className='text'>{task}</span>
+                <button
+                className='delete-button'
+                onClick={()=> deletTask(index)}>
+                    Delete
+
+                </button>
+                <button
+                className='move-button'
+                onClick={()=> moveTaskUp(index)}>
+                    👆
+                </button>
+
+                <button
+                className='move-button'
+                onClick={()=> moveTaskDown(index)}>
+                    👇
+                </button>
+            </li>)}
+        </ol>
+      
+    </div>
+  )
+}
+
+export default Todo
